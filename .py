9a4d2ca7d0673ee33c5d297318fb19dfd6bc8eb1
Ayanisnot <alayanhussain0@gmail.com>
2025-05-18 12:57:04 +0600

    Add files via upload

--- .py
+++ .py
@@ -1,5 +1,6 @@
-age = int(input("Enter the age :"))
-if 40 <= age <=60:
-      print("your age is between 40 and 60")
-else:
-      print("your age is not between 40 and 60")
+num = input("enter a number :")
+count = 0
+for ch in num:
+     if ch.isdigit():
+          count += 1
+print("total digits:" , count)
