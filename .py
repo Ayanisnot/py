61b8aabac4f5931de7aa3d6e7da504634473a5eb
Ayanisnot <alayanhussain0@gmail.com>
2025-06-06 00:23:18 +0600

    Add files via upload

--- .py
+++ .py
@@ -1,6 +1,13 @@
-def cube(number):
-    return number * number * number
+def shutdown(s) :
+    s = s.upper()
+    if s == "YES":
+        return  "shutting down..."
+    elif s == "NO":
+        return "shutdown aborted.." 
+    else :
+        return "SORRY, I DIDNT UNDERSTAND THAT."
+    
 
-number = 27
+print(shutdown("maybe")) 
 
-print(f"The cube of {number} is {cube(number)}")
+
