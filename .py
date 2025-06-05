33b8b60c02179fd778094a18fcd4ade220b78e2e
Ayanisnot <alayanhussain0@gmail.com>
2025-06-05 21:01:40 +0600

    Add files via upload

--- .py
+++ .py
@@ -1,11 +1,8 @@
-def weather_condition():
-   
-   print("weather is pleasant in",spring)
-   print("weather is same in",autumn)
+def total_calc(bill_amount, tip_perc=10):
+    total = bill_amount + 0.01*tip_perc*bill_amount
+    total = round(total , 2)
+    print(f"please pay ${total}")
 
 
-spring = "autumn"
-autumn = "winter" 
-
-weather_condition() 
-
+total_calc(150)
+total_calc(150, 20)
